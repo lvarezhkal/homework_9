182bd500163462de2f98e1025a007b65c0fdbfa9
lvarezhkal <kkakxaa@gmail.com>
2024-01-02 15:34:20 +0300

    allDone

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
         task3();
         task4();
     }
-
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
@@ -14,8 +13,8 @@ public class Main {
         }
         return arr;
     }
+    //1
     private static void task1 () {
-        //1
         int[] arr = generateRandomArray();
         int totalMoney = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -23,8 +22,8 @@ public class Main {
         }
         System.out.println("Сумма трат за месяц составила " + totalMoney + " рублей");
     }
+    //2
     private static void task2 () {
-        //2
         int[] arr = generateRandomArray();
         int min = arr[0];
         int max = arr[0];
@@ -38,9 +37,8 @@ public class Main {
         }
         System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей");
     }
-
+    //3
     private static void task3 () {
-      //3
         int[] arr = generateRandomArray();
         double totalMoney = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -49,9 +47,8 @@ public class Main {
         double average = totalMoney / arr.length;
         System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
     }
-
+    //4
     private static void task4 () {
-        //4
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
